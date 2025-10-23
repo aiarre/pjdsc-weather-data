d506ef18dc2fa5ca0518225bcf1c3f1954213b55
reuter <rjfc123.mny@gmail.com>
2025-10-23 22:07:50 +0800

    allowed cors in backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,11 @@ INSTALLED_APPS = [
     "rest_framework",
     "flood",
     "django_extensions",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -78,3 +80,5 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
