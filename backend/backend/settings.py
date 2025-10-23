fecd636d7820415189ac710c02e0913eea3ed41e
reuter <rjfc123.mny@gmail.com>
2025-10-23 20:45:24 +0800

    backend code ready to launch

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
